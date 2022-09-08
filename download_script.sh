61841fb81bc7697de31e389f9ae55220921b3f44
sateesh2954 <57485710+sateesh2954@users.noreply.github.com>
2022-09-08 21:24:50 +0530

    Update download_script.sh

--- download_script.sh
+++ download_script.sh
@@ -15,7 +15,7 @@ then
 else
  echo "Directory /opt/diy-migration-appliance/scripts created"
 fi
-for f in appliance.sh config config-with-values cronjobscheduler.sh ibmcloudloginstatus.sh linux_precheck.sh migration.sh windows_precheck.ps1 
+for f in appliance.sh config cronjobscheduler.sh ibmcloudloginstatus.sh linux_precheck.sh migration.sh windows_precheck.ps1 
  do
   response=$(curl -so ${f} -w "%{http_code}" https://raw.githubusercontent.com/IBM-Cloud/vpc-server-migration/main/server-migration-scripts/${f})
    if [ "$response" -eq "200" ]; then
