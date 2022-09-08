455664b49b04f039ac52afd0f22942c6c1148689
sateesh2954 <57485710+sateesh2954@users.noreply.github.com>
2022-09-08 21:11:07 +0530

    Update download_script.sh

--- download_script.sh
+++ download_script.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install nmap -y |
-sudo apt-get install qemu-kvm -y |
+sudo apt-get update -y 
+sudo apt-get install nmap -y 
+sudo apt-get install qemu-kvm -y 
 curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
-| ibmcloud plugin install vpc-infrastructure
-| ibmcloud plugin install cloud-object-storage
+ibmcloud plugin install vpc-infrastructure
+ibmcloud plugin install cloud-object-storage
 
-mkdir -p /opt/diy-migration-appliance/scripts
-cd /opt/diy-migration-appliance/scripts
+mkdir -p /opt/server-migration/scripts
+cd /opt/server-migration/scripts
 if [[ $? -ne 0 ]]
 then
- echo "Failed while creating directory /opt/diy-migration-appliance/scripts"
+ echo "Failed while creating directory /opt/server-migration/scripts"
  exit 1
 else
  echo "Directory /opt/diy-migration-appliance/scripts created"
 fi
-for f in Appliance-new.sh config config-with-values cronjobscheduler.sh ibmcloudloginstatus.sh linux_precheck.sh migration.sh windows_precheck.ps1 
+for f in appliance.sh config config-with-values cronjobscheduler.sh ibmcloudloginstatus.sh linux_precheck.sh migration.sh windows_precheck.ps1 
  do
-  response=$(curl -so ${f} -w "%{http_code}" https://raw.githubusercontent.com/IBM-Cloud/vpc-server-migration/tree/main/server-migration-scripts${f})
+  response=$(curl -so ${f} -w "%{http_code}" https://raw.githubusercontent.com/IBM-Cloud/vpc-server-migration/main/server-migration-scripts/${f})
    if [ "$response" -eq "200" ]; then
-    echo "${f} download complete - /opt/diy-migration-appliance/Scripts, http response code is: $response" >>/var/log/diy_download_script.log
+    echo "${f} download complete - /opt/diy-migration-appliance/Scripts, http response code is: $response" >>/var/log/download_script.log
    else
-    echo "Failed downloading  ${f} to /opt/diy-migration-appliance/Scripts, http response code is $response" >>/var/log/diy_download_script.log
+    echo "Failed downloading  ${f} to /opt/diy-migration-appliance/Scripts, http response code is $response" >>/var/log/download_script.log
     exit 1
    fi
  done
-echo "Updating /opt/diy-migration-appliance/Scripts permission" >>/var/log/diy_download_script.log
+echo "Updating /opt/server-migration/scripts permission" >>/var/log/download_script.log
 chmod +x *
